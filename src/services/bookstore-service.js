ad499cd5ed3de4ba87a7b45ceb73a19101623aba
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-13 21:26:55 +0300

    Класс-сервис: имитация backend

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,38 @@
+/*
+  Класс сервис, настоящий бы работал с backend данными
+*/
+
+export default class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: 'Production-Ready Microservices',
+      author: 'Susan J.Fowler',
+      price: 32,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 2,
+      title: 'Realese It!',
+      author: 'Michael T. Nygard',
+      price: 45,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg',
+    },
+  ];
+
+  // Возврат данные асинхронно
+  getBooks() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // 1/4 - случаях будет ошибка
+        if (Math.random() > 0.25) {
+          reject(new Error('Something bad happened!'));
+        } else {
+          resolve(this.data);
+        }
+      }, 800);
+    });
+  }
+}
