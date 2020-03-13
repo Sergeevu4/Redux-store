aa0f1436d75b9f44cba0da8be136b1401bee9a16
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-13 21:23:17 +0300

    Компонент: error-indicator

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './error-indicator.css';
+// import icon from './death-star.png'; // Webpack - кодирует в base64
+
+// # Компонент показа сообщения об ошибки
+const ErrorIndicator = () => {
+  return <div className='error-indicator'>Error!</div>;
+};
+
+export default ErrorIndicator;
