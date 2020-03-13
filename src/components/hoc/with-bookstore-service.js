6280127716ec4c0d31e5a8a903715201c5a7679a
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-13 21:23:43 +0300

    HOC: with-bookstore-service

---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BookstoreServiceConsumer } from '../bookstore-service-context';
+
+const withBookstoreService = (mapMethodsToProps) => (Wrapped) =>
+  function WithBookstoreService(props) {
+    return (
+      // Получаю доступ к Класс Сервису через внутреннюю функцию которая принимает сервис
+      // который мы передаем через BookstoreServiceProvider (REACT CONTEXT)
+      <BookstoreServiceConsumer>
+        {(bookstoreService) => {
+          return <Wrapped {...props} bookstoreService={bookstoreService} />;
+        }}
+      </BookstoreServiceConsumer>
+    );
+  };
+
+export default withBookstoreService;
