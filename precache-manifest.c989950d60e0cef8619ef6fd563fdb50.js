994644fdd2554504bfbdb3a73ca86b67ab1af6fc
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-27 14:59:34 +0300

    Updates

---
+++ precache-manifest.c989950d60e0cef8619ef6fd563fdb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a60bc19de0a1e9c176ff6680df080cf",
+    "url": "/Redux-store/index.html"
+  },
+  {
+    "revision": "13f47fb96ff64e0dbbc9",
+    "url": "/Redux-store/static/css/main.5b6d4d01.chunk.css"
+  },
+  {
+    "revision": "e9aaa4cbf4bcf3970767",
+    "url": "/Redux-store/static/js/2.0435f656.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Redux-store/static/js/2.0435f656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f47fb96ff64e0dbbc9",
+    "url": "/Redux-store/static/js/main.c3ff6af0.chunk.js"
+  },
+  {
+    "revision": "2826bad993c60d8bec5d",
+    "url": "/Redux-store/static/js/runtime-main.8964f763.js"
+  }
+]);
